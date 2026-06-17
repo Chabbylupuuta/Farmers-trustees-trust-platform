b1d90bede83225bc0530c8328a13f5c3bc18e7fe
Chabby the Samurai <109098605+Chabbylupuuta@users.noreply.github.com>
2026-06-17 12:51:34 +0200

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,1 +1,18 @@
+export const siteConfig = {
+  name: "Farmers & Trustees Trust",
+  url: "https://ftt.co.zm",
 
+  phone: "+260975963056",
+
+  whatsapp: "260975963056",
+
+  email: "info@ftt.co.zm",
+
+  address: "Lusaka, Zambia",
+
+  socials: {
+    facebook: "#",
+    linkedin: "#",
+    twitter: "#",
+  },
+};
